34cc809d4735ccd1a89984c268b1d72f4a37594d
seohyun319 <tjguselly0319@gmail.com>
2022-02-04 19:48:16 +0900

    FEAT: add css on MainPage and NeedPage

--- icying/src/components/NeedPage/MainPage.js
+++ icying/src/components/NeedPage/MainPage.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import NeedPage from './NeedPage'
+import NeedPage from './NeedPage';
+import './NeedPage.css';
 
 function MainPage() {
 
   return (
-    <div>
-      <div>ICYING SAVES GREEN🧊</div>
-      <div>
+    <div className='MainPage'>
+      <div className='mainTitle'>ICYING SAVES GREEN🧊</div>
+      <div className='mainTxt'>
         <p>누군가는 사고, 누군가는 버리는 아이스팩. <br />
         아이싱은 전 세계가 직면한 기후 변화 문제의 쿠키 위에<br />
         아이스팩 리사이클링을 올려 지속 가능한 자원 순환 경제를 실현하고자 합니다.</p>
--- icying/src/components/NeedPage/NeedPage.js
+++ icying/src/components/NeedPage/NeedPage.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import PlaceList from './PlaceList';
 import PlaceRead from './PlaceRead';
+import './NeedPage.css';
 
 function NeedPage() {
 
   return (
-    <div>
-      <div>내 주변 아이스팩 필요 매장</div>
-      <button>매장 등록</button>
+    <div className='blueContainer'>
+      <div className='flexBetween'>
+        <div className='blueTxt'>내 주변 아이스팩 필요 매장</div>
+        <button className='whiteBtn'>매장 등록</button>
+      </div>
       <PlaceList />
       <PlaceRead />
     </div>
