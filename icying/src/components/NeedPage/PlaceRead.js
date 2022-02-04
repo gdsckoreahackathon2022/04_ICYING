a03105570ea3a363734767a6e07044220cd5a33c
seohyun319 <tjguselly0319@gmail.com>
2022-02-05 08:42:44 +0900

    FEAT: get DB on PlaceRead

--- icying/src/components/NeedPage/PlaceRead.js
+++ icying/src/components/NeedPage/PlaceRead.js
@@ -4,49 +4,34 @@ import { token } from '../Config'
 import './NeedPage.css';
 
 function PlaceRead() {
-  const places = [
-    {
-      restaurant_id: 1,
-      name: "몬스터플레이스 숙대점",
-      address: "서울특별시 용산구 청파동 청파로43길 70",
-      latitude: 126.19893898,
-      longitude: 36.1231412,
-      ice_need_number: "800",
-      logo_image_url: "https://example.com/image.png",
-      descript: "안녕하세요! 몬스터플레이스입니다~"
-    }
-  ];
   const baseUrl = process.env.REACT_APP_BASE_URL;
-  // const [places, setPlaces] = useState("");
+  const [place, setPlace] = useState("");
   const getData = async () => {
     const headers = {
       'Authorization': token
     }
     const response = await axios.get(
-      baseUrl + "/restaurant/1", {headers}     
+      baseUrl + "/restaurant/3", {headers}     
     );
-    console.log(response.data.response)
-    console.log(response.data.response.name)
-    // setPlaces(places, ...response.data.response)
+    setPlace(response.data.response)
   };  
   useEffect(() => {
     getData();
   }, []);
-  const renderPlaces = places && places.map(place => {
-    const ice_num = place.ice_need_number == "free" ? "많이" : place.ice_need_number+"개";
-    return (
-      <div className='PlaceReadContainer' key={place.restaurant_id}>
-        <img className='readImg' src='{place.logo_image_url}'></img>
-        <div className='placeReadName'>{place.name}</div>
-        <div className='placeAddr'>{place.address}</div>
-        <div className='needNumTxt'><p>{ice_num}</p> 필요해요!</div>          
-        <div>{place.descript}</div>
-      </div>
-    )
-  })
+
+  const ice_num = place.ice_need_number == "free" ? "많이" : place.ice_need_number+"개";
+  
   return (
     <div className='PlaceRead'>
-      <div>{renderPlaces}</div>
+      <div>
+        <div className='PlaceReadContainer' key={place.restaurant_id}>
+          <img className='readImg' src={place.logo_image_url}></img>
+          <div className='placeReadName'>{place.name}</div>
+          <div className='placeAddr'>{place.address}</div>
+          <div className='needNumTxt'><p>{ice_num}</p> 필요해요!</div>          
+          <div>{place.descript}</div>
+        </div>
+      </div>
     </div>
   )
 }
