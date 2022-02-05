b42c00e367c98739328a73546c25a1e80503f87c
seohyun319 <tjguselly0319@gmail.com>
2022-02-05 10:07:47 +0900

    FEAT: get DB on Reward and MyIcyingPage

--- icying/src/components/RewardPage/RewardPage.js
+++ icying/src/components/RewardPage/RewardPage.js
@@ -1,47 +1,38 @@
-import React, { useEffect } from 'react';
-import { useState } from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { token } from '../Config'
 import "./RewardPage.css";
 
 function RewardPage() {
-    const posts1 = [
-        {
-          "index": 1,
-          "restaurant": {
-            "restaurant_id": 1,
-            "name": "몬스터플레이스 숙대점",
-            "logo_image_url": "https://s3-example.com/bucket/object.png"
-          },
-          "ice_number": 100,
-          "created_at": "2021-06-25"
-        },
-        {
-          "index": 2,
-          "restaurant": {
-            "restaurant_id": 1,
-            "name": "몬스터플레이스 숙대점",
-            "logo_image_url": "https://s3-example.com/bucket/object.png"
-          },
-          "ice_number": 100,
-          "created_at": "2021-06-25"
-        },
-        {
-          "index": 3,
-          "restaurant": {
-            "restaurant_id": 1,
-            "name": "몬스터플레이스 숙대점",
-            "logo_image_url": "https://s3-example.com/bucket/object.png"
-          },
-          "ice_number": 100,
-          "created_at": "2021-06-25"
+      const baseUrl = process.env.REACT_APP_BASE_URL;
+      const [posts1, setPosts1] = useState("");
+      const [posts2, setPosts2] = useState("");
+      const getData = async () => {
+        const headers = {
+          'Authorization': token
         }
-      ];
+        const response = await axios.get(
+          baseUrl + "/rewards/medal/", {headers}     
+        );
+        setPosts2(response.data.response.rewards)
+        console.log(response.data.response.rewards)
+
+        const response2 = await axios.get(
+          baseUrl + "/rewards/record/?display=3&query=biggest", {headers}     
+        );
+        setPosts1(response2.data.response.records)
+        console.log(response2.data.response.records)
+      };  
+      useEffect(() => {
+        getData();
+      }, []);
 
       const renderPosts1 = posts1 && posts1.map(post => {
           return (
               <div className = 'post' key={post.index}>
                 <div className="recordContainer">
-                <img className="resimg" src={post.logo_image_url}/>               
-                <div className="icenum">{post.ice_number}개</div>
+                <img className="resimg" src={post.restaurant.logo_image_url}/>               
+                <div className="icenum">{post.ice_pack_number}개</div>
                 <div className="resname">{post.restaurant.name}</div>
                 <div className="icedate">{post.created_at}</div>
                 
@@ -50,62 +41,12 @@ function RewardPage() {
           )
       })
 
-      const posts2 = [
-          {
-            "index": 1,
-            "code": "re1",
-            "content": "아기 뱃지 획득!",
-            "number": 1,
-            "is_exist": true,
-            "created_at": "2021-06-25"
-          },
-          {
-            "index": 2,
-            "code": "re1",
-            "content": "기부 10개",
-            "number": 10,
-            "is_exist": false,
-            "created_at": "null"
-          },
-          {
-            "index": 3,
-            "code": "re1",
-            "content": "기부 10개",
-            "number": 10,
-            "is_exist": false,
-            "created_at": "null"
-          },
-          {
-            "index": 4,
-            "code": "re1",
-            "content": "기부 10개",
-            "number": 10,
-            "is_exist": false,
-            "created_at": "null"
-          },
-          {
-            "index": 5,
-            "code": "re1",
-            "content": "기부 10개",
-            "number": 10,
-            "is_exist": false,
-            "created_at": "null"
-          },
-          {
-            "index": 6,
-            "code": "re1",
-            "content": "기부 10개",
-            "number": 10,
-            "is_exist": false,
-            "created_at": "null"
-          }
-        ]
 
         const renderPosts2 = posts2 && posts2.map(post => {
             return (
                 <div className = 'post' key={post.index}>
                   <div className="recordContainer">
-                  <img className="medalimg" src={post.logo_image_url}/>               
+                  <img className="medalimg" src={post.badge}/>               
                   <div className="badgename">{post.content}</div>
                   <div className='icedate'>{post.created_at}</div>
                   </div>
